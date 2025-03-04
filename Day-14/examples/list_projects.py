3065b6595b8acb97e507d0c01536de2421e66aba
Rohit Bhushan <58414896+rohit106bhushan@users.noreply.github.com>
2025-03-04 14:49:47 +0530

    Update list_projects.py

--- Day-14/examples/list_projects.py
+++ Day-14/examples/list_projects.py
@@ -4,11 +4,11 @@ import requests
 from requests.auth import HTTPBasicAuth
 import json
 
-url = "https://veeramallaabhishek.atlassian.net/rest/api/3/project"
+url = "https://rohit106bhushan.atlassian.net/rest/api/3/project"
 
-API_TOKEN=""
+API_TOKEN="ATATT3xFfGF0cy4DtYIxVxRIysCzmZV_YEEYqrnfUaspaimGnWLVhJ8iAGB5EbsWoGBF9REnU7p3t9dvWYlLWIrJK3tSiyLjYgCQnyD4h65vXWHArgLullt-Y77eLW1VI1qHf1Tc2aungw1XMaU6OCEQF3mMJEGf0kaYLUN3-asMKi4m2DbpK_4=8C2724AB"
 
-auth = HTTPBasicAuth("", API_TOKEN)
+auth = HTTPBasicAuth("rohit106bhushan@gmail.com", API_TOKEN)
 
 headers = {
   "Accept": "application/json"
