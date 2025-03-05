9f85ebaafe883c99673ce64b6c1a477ed9bd18d0
Rohit Bhushan <58414896+rohit106bhushan@users.noreply.github.com>
2025-03-05 07:02:53 +0530

    Update list_projects.py
    removed api token

--- Day-14/examples/list_projects.py
+++ Day-14/examples/list_projects.py
@@ -6,7 +6,7 @@ import json
 
 url = "https://rohit106bhushan.atlassian.net/rest/api/3/project"
 
-API_TOKEN="ATATT3xFfGF0cy4DtYIxVxRIysCzmZV_YEEYqrnfUaspaimGnWLVhJ8iAGB5EbsWoGBF9REnU7p3t9dvWYlLWIrJK3tSiyLjYgCQnyD4h65vXWHArgLullt-Y77eLW1VI1qHf1Tc2aungw1XMaU6OCEQF3mMJEGf0kaYLUN3-asMKi4m2DbpK_4=8C2724AB"
+
 
 auth = HTTPBasicAuth("rohit106bhushan@gmail.com", API_TOKEN)
 
